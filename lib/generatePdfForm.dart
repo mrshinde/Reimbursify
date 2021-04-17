87f7a8c3d595b7e29516fcb40fdbc6e6030005f9
i-navneet <60032662+i-navneet@users.noreply.github.com>
2021-04-17 16:55:57 +0530

    Header of PDF

--- lib/generatePdfForm.dart
+++ lib/generatePdfForm.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/services.dart';
 import 'package:tripmanager/classes/profileclass.dart';
 import 'package:tripmanager/classes/formClass.dart';
 import 'package:tripmanager/classes/tripclass.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 Future<int> createPdf(int tripid) async{
   FormClass form = FormClass(tripid);
@@ -16,20 +20,36 @@ Future<int> createPdf(int tripid) async{
     title: tripInfo.title + tripInfo.start_date,
   );
 
+  final iit_logo = (await rootBundle.load('assets/iit.png')).buffer.asUint8List();
+
   pdf.addPage(
     pw.MultiPage(
       pageFormat: PdfPageFormat.a4,
       build: (pw.Context context){
         return <pw.Widget>[
-          createHeader(),
+          createHeader(iit_logo),
         ];
       },
     )
   );
+  Directory directoryPath = await getApplicationDocumentsDirectory();
+  String docPath = directoryPath.path;
+  print(docPath);
+  var targetName = tripInfo.title + tripInfo.start_date;
+  File filename = File('$docPath/$targetName.pdf');
+  pdf.save().then((value){filename.writeAsBytesSync(value);});
 }
 
-pw.Widget createHeader(){
+pw.Widget createHeader(var imgData){
   return pw.Table(
-    // TODO: create a table form widget with IIT Logo and Name
+    columnWidths: {0: pw.FractionColumnWidth(0.2), 1: pw.FractionColumnWidth(0.8)},
+    children: <pw.TableRow>[
+      pw.TableRow(
+        children: <pw.Widget>[
+          pw.Image(pw.MemoryImage(imgData)),
+          pw.Text('Indian Institute of Technology Ropar'),
+        ],
+      ),
+    ],
   );
 }
