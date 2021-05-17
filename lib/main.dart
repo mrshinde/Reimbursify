6c2ee460d810b11810f3f8db058b9d3a3a147f18
Mohit Rajoria <mohit.raj.khetri@gmail.com>
2021-05-17 20:02:27 +0530

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tripmanager/Home/firstsignup.dart';
 import 'package:tripmanager/Home/signup.dart';
